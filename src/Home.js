ae86109b9cf0d86824e2a0d7ea39541730fe9520
Chris Jiang <chrjiang@nvidia.com>
2021-07-24 13:12:30 +0800

    [feat] handle events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('hello bitch', e)
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello again ' + name, e.target)
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      {/* could remove the curly brackets around handleClickAgain.  */}
+      <button onClick={(e) => { handleClickAgain('Chris', e) }}> Another Click</button> 
     </div>
   );
 }
- 
+
 export default Home;
