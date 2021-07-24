322a6eef35a350443e4daf89cdaa2206544c0f50
Chris Jiang <chrjiang@nvidia.com>
2021-07-24 16:47:16 +0800

    handling, throwing and showing errors

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,7 @@ const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [name, setName] = useState('mario')
+  const [error, setError] = useState(null);
   /*should not hardcoding the lists, need a way to cycle through the list
   However, this really repetivie and needs a template to make it reusable
   So, use props to pass as arguments
@@ -18,13 +19,23 @@ const Home = () => {
   */
 
   const FetchAndUpdateData = () => {
+    //fetch('http://localhost:8000/blogs22')
     fetch('http://localhost:8000/blogs')
       .then(res => {
+        //console.log(res)
+        if (!res.ok){
+          throw Error('Could not fetch the data, data is null');
+        }
         return res.json();
       })
       .then(data => {
         console.log(data);
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err)=>{
+        setError(err.message);
         setIsPending(false);
       });
   }
@@ -54,6 +65,7 @@ const Home = () => {
   return (
     <div className="home">
       {isPending && <div>Loading ...</div>}
+      {error && <div>{error}</div> }
       {blogs && <BlogList blogs={blogs} title={title} />}
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title = 'Noob' /> */}
       {/* <button onClick={()=>setName('Chris, new name')}>change Name</button> */}
