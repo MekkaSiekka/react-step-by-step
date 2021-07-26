eec5830f5800a17aa3130da13d8feaf94196e0bf
MekkaSiekka <jiangxiao961123@gmail.com>
2021-07-26 23:03:12 +0800

    router, route and switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+//surround the code using router
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/anotherhome">
+            <Home></Home>
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
